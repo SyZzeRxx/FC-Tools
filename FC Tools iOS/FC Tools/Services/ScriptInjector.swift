e160d9e538d077e9f603d9a4e51b3989f08bec84
SyZzeRxx <auckkxrh@gmail.com>
2026-07-31 16:17:12 +0300

    Update ScriptInjector.swift

--- FC Tools iOS/FC Tools/Services/ScriptInjector.swift
+++ FC Tools iOS/FC Tools/Services/ScriptInjector.swift
@@ -18,6 +18,7 @@ struct ScriptInjector {
     static let marker = "__fcToolsFodderInjected"
     static let updateURL = URL(string: "https://fodder.gg/fodder.user.js")!
 
+
     func source() throws -> String {
         if let updated = try? String(contentsOf: ScriptUpdateService.cachedScriptURL, encoding: .utf8),
            ScriptUpdateService.isValid(updated) {
